9192ef2bf8819c2aec762ecd0e3bb1c1e0fe86b7
Aurelio <aurelio.iempereur@gmail.com>
2025-08-03 22:12:43 -0600

    tester

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,4 @@ fn main() {
     let mut token_two = tokenization::tokenization("( 2 * 2 - 8 ) + 15 > 9 + 9");
     let operator_two = tree_generator::parse_expression(&mut token_two);
     println!("{:?}",operator_two);
-    // tokenization::parse_expression(&mut token_one);
-    // print!("\n");
-    //************************************************
-    //let mut tokens = tokenization::tokenization("( 2 *2 - 8 ) + 15");
-    //print!("\n");
-    // let operator = tokenization::parse_expression(&mut tokens);
-    // print!("\n");
-    // let result = logical_evaluations::operation_arithmeticals(&operator);
-    // print!("Final Result : {}",result);
 }
